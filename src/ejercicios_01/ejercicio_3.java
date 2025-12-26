21d5bc7a220045fa451c3499ef4d767175f5a2d9
edgar <edgar_rame98@outlook.com>
2025-12-26 12:15:35 -0600

    Impresion de 10 palabras en ingles con significado en español

---
+++ src/ejercicios_01/ejercicio_3.java
@@ -0,0 +1,41 @@
+package ejercicios_01;
+
+public class ejercicio_3 {
+    static void main() {
+        //Zona de variables (palabras en ingles)
+        String word1 = "apple";
+        String word2 = "pen";
+        String word3 = "chiken";
+        String word4 = "tree";
+        String word5 = "water";
+        String word6 = "actually";
+        String word7 = "armchair";
+        String word8 = "paper";
+        String word9 = "label";
+        String word10 = "number";
+
+        //Zona de variables (palabras en español)
+        String palabra1 = "manzana";
+        String palabra2 = "pluma";
+        String palabra3 = "pollo";
+        String palabra4 = "árbol";
+        String palabra5 = "agua";
+        String palabra6 = "de hecho";
+        String palabra7 = "sillon";
+        String palabra8 = "papel";
+        String palabra9 = "capa";
+        String palabra10 = "numero";
+
+        //Código principal (impresión de palabras)
+        System.out.println(word1 + " significa: " + palabra1);
+        System.out.println(word2 + " significa: " + palabra2);
+        System.out.println(word3 + " significa: " + palabra3);
+        System.out.println(word4 + " significa: " + palabra4);
+        System.out.println(word5 + " significa: " + palabra5);
+        System.out.println(word6 + " significa: " + palabra6);
+        System.out.println(word7 + " significa: " + palabra7);
+        System.out.println(word8 + " significa: " + palabra8);
+        System.out.println(word9 + " significa: " + palabra9);
+        System.out.println(word10 + " significa: " + palabra10);
+    }
+}
