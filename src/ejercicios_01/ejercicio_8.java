b283c6951ad276dc65e8bbcb5671c07f0d57ae66
edgar <edgar_rame98@outlook.com>
2025-12-26 14:05:53 -0600

    Impresión de piramide hueca inversa

---
+++ src/ejercicios_01/ejercicio_8.java
@@ -0,0 +1,11 @@
+package ejercicios_01;
+
+public class ejercicio_8 {
+    static void main() {
+        System.out.println("*********");
+        System.out.println(" *     * ");
+        System.out.println("  *   *  ");
+        System.out.println("   * *   ");
+        System.out.println("    *    ");
+    }
+}
