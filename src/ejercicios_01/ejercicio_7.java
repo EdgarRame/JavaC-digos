408e74572e49aba30e8a170ff5c29bbfbbd863a1
edgar <edgar_rame98@outlook.com>
2025-12-26 14:00:59 -0600

    Impresión de piramide hueca

---
+++ src/ejercicios_01/ejercicio_7.java
@@ -0,0 +1,11 @@
+package ejercicios_01;
+
+public class ejercicio_7 {
+    static void main() {
+        System.out.println("    *    ");
+        System.out.println("   * *   ");
+        System.out.println("  *   *  ");
+        System.out.println(" *     * ");
+        System.out.println("*********");
+    }
+}
