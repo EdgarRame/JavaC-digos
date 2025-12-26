d944a461ee95407e4c8f14387e0fc29f980d1693
edgar <edgar_rame98@outlook.com>
2025-12-26 12:13:22 -0600

    Imprimir nombre en consola

---
+++ src/ejercicios_01/ejercicio_1.java
@@ -0,0 +1,11 @@
+package ejercicios_01;
+
+public class ejercicio_1 {
+    static void main() {
+        //Zona de variables
+        String nombre = "Edgar Ramírez Medina";
+        //Zona de código
+        System.out.println("Programa que muestra tu nombre en consola");
+        System.out.println("Tu nombre es: " + nombre);
+    }
+}
