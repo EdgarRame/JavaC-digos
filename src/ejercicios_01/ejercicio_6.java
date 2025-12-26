e21507427dc7eada96a52f01629b0b9e87c8f344
edgar <edgar_rame98@outlook.com>
2025-12-26 13:58:50 -0600

    Corrección del código

--- src/ejercicios_01/ejercicio_6.java
+++ src/ejercicios_01/ejercicio_6.java
@@ -3,19 +3,10 @@ package ejercicios_01;
 public class ejercicio_6 {
     static void main() {
         //Piramides de asteriscos
-        //Variables
-        int numeroLimite = 5;
-
-        for (int altura = 1; altura < 6; altura++) {
-            //Espacios en blanco
-            for (int blanco = 1; blanco < numeroLimite - altura; blanco++) {
-                System.out.print(" ");
-            }
-            //Impresión de asteriscos
-            for (int asteriscos = 1; asteriscos <=(altura*2)-1 ; asteriscos++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+        System.out.println("    *    ");
+        System.out.println("   ***   ");
+        System.out.println("  *****  ");
+        System.out.println(" ******* ");
+        System.out.println("*********");
     }
 }
