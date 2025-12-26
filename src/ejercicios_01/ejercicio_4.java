55a31d2430aba24f280d9168d22730421122522a
edgar <edgar_rame98@outlook.com>
2025-12-26 12:16:17 -0600

    Impresión de horario

---
+++ src/ejercicios_01/ejercicio_4.java
@@ -0,0 +1,13 @@
+package ejercicios_01;
+
+public class ejercicio_4 {
+    static void main() {
+        //Parte principal del código
+        System.out.println("             \t\t\t\t\t\t\t\t\t***\tHorario\t***");
+        //Horario con materias
+        System.out.println("Materia       \t\t\tLunes\t\t\t\tMartes\t\t\t Miercoles\t\t\t Jueves\t\t\t Viernes");
+        System.out.println("Sistemas Programables\t 09:00-10:00\t\t09:00-10:00\t\t09:00-10:00\t\t09:00-10:00\t\t09:00-10:00\t\t09:00-10:00");
+        System.out.println("BD NoSQL\t\t\t     15:00-16:00\t\t15:00-16:00\t\t15:00-16:00\t\t15:00-16:00\t\t15:00-16:00");
+        System.out.println("Int. Artificial\t\t     10:00-11:00\t\t10:00-11:00\t\t10:00-11:00\t\t10:00-11:00");
+    }
+}
