3f20563d1245ceb32ba99824bb65a505a2e913bd
edgar <edgar_rame98@outlook.com>
2025-12-26 01:43:49 -0600

    Coloreado de textos en consola

---
+++ src/TextoaColor/textocoloreado.java
@@ -0,0 +1,17 @@
+package TextoaColor;
+
+public class textocoloreado {
+    static void main() {
+        String rojo = "\033[31m";
+        String verde = "\033[32m";
+        String naranja = "\033[33m";
+        String azul = "\033[34m";
+        String morado = "\033[35m";
+        String blanco = "\033[36m";
+
+        System.out.println(naranja + "mandarina" + verde);
+        System.out.println(naranja + "saltamonetes" + rojo + "tomate");
+        System.out.println(blanco + "sábanas" + azul + "cielo");
+        System.out.println(morado + "nazareno" + azul + "mar");
+    }
+}
