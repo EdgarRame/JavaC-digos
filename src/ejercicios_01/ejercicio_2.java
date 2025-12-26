97e45611500ebee027bfec10fc2ace191ee7e9df
edgar <edgar_rame98@outlook.com>
2025-12-26 12:14:33 -0600

    Impresion de nombre y telefono en consola

---
+++ src/ejercicios_01/ejercicio_2.java
@@ -0,0 +1,12 @@
+package ejercicios_01;
+
+public class ejercicio_2 {
+    static void main() {
+        //Zona de variables
+        String nombre = "Edgar Ramírez Medina";
+        String num_tel = "1234567890";
+        //Zona del código
+        System.out.println("Tu nombre es: " + nombre);
+        System.out.println("Tu número telefonico es: " + num_tel);
+    }
+}
