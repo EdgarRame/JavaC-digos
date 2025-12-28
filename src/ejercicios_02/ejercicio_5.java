51d5baf688f9dea7e9f2840995fdccf19f0f7674
edgar <edgar_rame98@outlook.com>
2025-12-28 13:35:00 -0600

    Uso de variables para hacer el cambió de monedas en forma invertida

---
+++ src/ejercicios_02/ejercicio_5.java
@@ -0,0 +1,13 @@
+package ejercicios_02;
+
+public class ejercicio_5 {
+    static void main() {
+        //Zona de variables
+        double val_Eu = 166386;
+        int cant_Pese = 5;
+        double cambio = cant_Pese/val_Eu*100;
+
+        //Zona del código
+        System.out.println("La cantidad de euros que tendras son: " + cambio);
+    }
+}
