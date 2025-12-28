373702f26fe345c9a2aa684171f412298b076edb
edgar <edgar_rame98@outlook.com>
2025-12-28 13:29:49 -0600

    Uso de variables para hacer operaciones

---
+++ src/ejercicios_02/ejercicio_1.java
@@ -0,0 +1,21 @@
+package ejercicios_02;
+
+public class ejercicio_1 {
+    static void main() {
+        //Zona de variables
+        int x = 144;
+        int y = 999;
+        int sum = x+y;
+        int resta = x-y;
+        int mul = x*y;
+        int div = x/y;
+
+        //Zona del código
+        System.out.println("El valor de X es = " + x);
+        System.out.println("El valor de Y es = " + y);
+        System.out.println("El resultado de la suma es = " + sum);
+        System.out.println("El resultado de la resta es = " + resta);
+        System.out.println("El resultado de la multiplicación es = " + mul);
+        System.out.println("El resultado de la división es = " + div);
+    }
+}
