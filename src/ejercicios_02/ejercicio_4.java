7d150148185b582b85ba31434c2d9cd365d0221e
edgar <edgar_rame98@outlook.com>
2025-12-28 13:34:25 -0600

    Uso de variables para hacer el cambió de monedas

---
+++ src/ejercicios_02/ejercicio_4.java
@@ -0,0 +1,13 @@
+package ejercicios_02;
+
+public class ejercicio_4 {
+    static void main() {
+        //Zona de variables
+        int val_Pese = 166386;
+        int cant_Eu = 5;
+        double cambio = cant_Eu*val_Pese;
+
+        //Zona del código
+        System.out.println("La cantidad de euros que tendras son: " + cambio);
+    }
+}
