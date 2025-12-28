53e3f832a6cd588ee547db0f3f321182a10b851b
edgar <edgar_rame98@outlook.com>
2025-12-28 13:32:42 -0600

    Uso de variables para imprimir diferentes datos

---
+++ src/ejercicios_02/ejercicio_3.java
@@ -0,0 +1,15 @@
+package ejercicios_02;
+
+public class ejercicio_3 {
+    static void main() {
+        //Zona de variables
+        String nombre = "Edgar Ramírez Medina";
+        String direccion = "Dirección #N°";
+        String telefono = "1234567890";
+
+        //Zona de código
+        System.out.println("Mi nombre es: " + nombre);
+        System.out.println("Mi dirección es: " + direccion);
+        System.out.println("Mi telefono es: " + telefono);
+    }
+}
