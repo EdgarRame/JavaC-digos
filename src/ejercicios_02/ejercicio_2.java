c1af96b03c45fd6742a644e96fc162aafd064ac0
edgar <edgar_rame98@outlook.com>
2025-12-28 13:31:35 -0600

    Uso de variables para imprimir cadenas

---
+++ src/ejercicios_02/ejercicio_2.java
@@ -0,0 +1,10 @@
+package ejercicios_02;
+
+public class ejercicio_2 {
+    static void main() {
+        //Zona de variables
+        String nombre = "Edgar Ramírez Medina";
+        //Zona de código
+        System.out.println("Mi nombre es: " + nombre);
+    }
+}
